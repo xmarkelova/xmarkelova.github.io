62e5418d1796a95414e6fd2744047c55842109dd
kilit <kilit31@gmail.com>
2021-01-21 23:47:40 +0300

    scroll

--- js/script.js
+++ js/script.js
@@ -203,4 +203,50 @@ btnShowMore.addEventListener('click', (evt) => {
         item.classList.remove('hide');
     }
     );
-});;
+});
+
+// scroll
+
+const scrolling = () => {
+
+
+    let links = document.querySelectorAll('[href^="#'),
+        speed = 0.45;
+
+    links.forEach(link => {
+        link.addEventListener('click', function (evt) {
+            evt.preventDefault();
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock)
+                        : Math.min(widthTop + progress / speed, widthTop + toBlock));
+
+                document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
+
+
+
+};
+
+scrolling();
+    // скролл страницы
+;
